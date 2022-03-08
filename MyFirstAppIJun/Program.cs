add67cb91877b8fdf2ac5810901e3abfd4039562
Migelius <Migelius@DESKTOP-USK661S>
2022-03-09 01:58:17 +0300

    Добавьте файлы проекта.

---
+++ MyFirstAppIJun/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte money;
+            sbyte gold;
+            short silver;
+            ushort diamond;
+            int arrow;
+            uint pills;
+            long bullet;
+            ulong scorePoints;
+            decimal damage;
+            float hitPoint;
+
+
+
+
+        }
+
+
+    }
+
+
+}
